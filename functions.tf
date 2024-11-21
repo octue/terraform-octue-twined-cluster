61e1c8f84f319276564c1d8b8aa10e3ffe70cfa0
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 16:59:57 +0000

    FIX: Fix name of event handler block

--- functions.tf
+++ functions.tf
@@ -1,4 +1,4 @@
-resource "google_cloudfunctions2_function" "service_event_handler" {
+resource "google_cloudfunctions2_function" "event_handler" {
   name        = "${var.twined_service_namespace}-octue-twined-service-event-handler"
   description = "A function for handling events from Octue Twined services."
   location    = var.google_cloud_region
