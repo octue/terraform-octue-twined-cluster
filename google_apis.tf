a91a24a14c1bb5fbe9181f798f42d7eca9f5299c
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 18:09:59 +0000

    FIX: Wait for Google APIs to be enabled before continuing

--- google_apis.tf
+++ google_apis.tf
@@ -19,3 +19,9 @@ resource "google_project_service" "google_apis" {
   disable_dependent_services = true
   project = var.google_cloud_project_id
 }
+
+
+resource "time_sleep" "wait_for_google_apis_to_enable" {
+  depends_on = google_project_service.google_apis
+  create_duration = "1m"
+}
