060ea3ca7b32b93bf0dbc71def8597117a32ec4a
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 13:01:20 +0000

    ENH: Add BigQuery table

---
+++ bigquery.tf
@@ -0,0 +1,96 @@
+resource "google_bigquery_dataset" "octue_service_events_dataset" {
+  dataset_id                  = "${var.octue_service_namespace}_octue_service_events"
+  description                 = "A dataset for storing Octue service events."
+  location                    = "EU"
+}
+
+resource "google_bigquery_table" "octue_service_events_table" {
+  table_id   = "service-events"
+  dataset_id = google_bigquery_dataset.octue_service_events_dataset.dataset_id
+  clustering = ["sender", "question_uuid"]
+
+  schema = <<EOF
+[
+  {
+    "name": "datetime",
+    "type": "TIMESTAMP",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "uuid",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "kind",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "event",
+    "type": "JSON",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "other_attributes",
+    "type": "JSON",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "originator",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "parent",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "sender",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "sender_type",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "sender_sdk_version",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "recipient",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "originator_question_uuid",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "parent_question_uuid",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "question_uuid",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "backend",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "backend_metadata",
+    "type": "JSON",
+    "mode": "REQUIRED"
+  }
+]
+EOF
+}
