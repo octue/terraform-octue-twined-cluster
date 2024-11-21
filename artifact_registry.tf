43a41780da08459e1d92b46ff5027c72371a0573
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 17:48:55 +0000

    FIX: Conform to artifact registry repository naming convention

--- artifact_registry.tf
+++ artifact_registry.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "service_docker_images" {
-  repository_id = "${var.twined_service_namespace}_octue_twined_services"
+  repository_id = "${var.twined_service_namespace}-octue-twined-services"
   description   = "Docker image repository for Octue Twined services"
   format        = "DOCKER"
   location      = var.google_cloud_region
