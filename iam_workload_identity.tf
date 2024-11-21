2b2e2851a828dedf0aad7bef42d19bdfdfa16e32
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 15:22:03 +0000

    ENH: Add IAM workload identity federation

---
+++ iam_workload_identity.tf
@@ -0,0 +1,42 @@
+resource "google_iam_workload_identity_pool" "github_actions_pool" {
+  workload_identity_pool_id = "github-actions-pool"
+  display_name              = "github-actions-pool"
+  project                   = var.google_cloud_project_id
+}
+
+
+resource "google_iam_workload_identity_pool_provider" "github_actions_provider" {
+    workload_identity_pool_id          = "github-actions-pool"
+    workload_identity_pool_provider_id = "github-actions-provider"
+    display_name                       = "Github Actions Provider"
+    project                            = var.google_cloud_project_number
+
+    attribute_mapping                  = {
+        "attribute.actor"            = "assertion.actor"
+        "attribute.repository"       = "assertion.repository"
+        "attribute.repository_owner" = "assertion.repository_owner"
+        "google.subject"             = "assertion.sub"
+    }
+
+    oidc {
+        allowed_audiences = []
+        issuer_uri        = "https://token.actions.githubusercontent.com"
+    }
+}
+
+
+data "google_iam_policy" "workload_identity_pool_policy" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+    members = [
+      "principalSet://iam.googleapis.com/projects/${var.google_cloud_project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_actions_pool.workload_identity_pool_id}/attribute.repository_owner/${var.github_organisation}"
+    ]
+  }
+}
+
+
+# Allow a machine under Workload Identity Federation to act as the given service account.
+resource "google_service_account_iam_policy" "workload_identity_service_account_policy" {
+  policy_data        = data.google_iam_policy.workload_identity_pool_policy.policy_data
+  service_account_id = google_service_account.github_actions_service_account.name
+}
