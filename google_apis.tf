f8f83b52b5e80794e98c47f171254e8a7021d183
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 17:31:54 +0000

    REF: Factor Google APIs to use out into local variable

--- google_apis.tf
+++ google_apis.tf
@@ -1,40 +1,20 @@
-# Artifact Registry is used to store docker images of the Twined services.
-resource "google_project_service" "artifact_registry" {
-  project = var.google_cloud_project_id
-  service = "artifactregistry.googleapis.com"
+locals {
+  apis = toset(
+    [
+      "artifactregistry.googleapis.com",  # Artifact Registry is used to store docker images of the Twined services.
+      "bigquery.googleapis.com",          # BigQuery provides the event store for Twined service events (questions, results, log messages etc.).
+      "cloudfunctions.googleapis.com",    # Cloud Functions runs the event handler that directs Twined service events to the event store.
+      "run.googleapis.com",               # Cloud Run runs the Twined services as docker containers.
+      "iam.googleapis.com",               # IAM provides fine-grained authentication and authorisation to use and access the Twined services and input/output data.
+      "pubsub.googleapis.com"             # Pub/Sub is the transport mechanism for Twined service events.
+    ]
+  )
 }
 
 
-# BigQuery provides the event store for Twined service events (questions, results, log messages etc.).
-resource "google_project_service" "bigquery" {
+resource "google_project_service" "google_apis" {
+  for_each = locals.apis
+  service = each.key
   project = var.google_cloud_project_id
-  service = "bigquery.googleapis.com"
-}
-
-
-# Cloud Functions runs the event handler that directs Twined service events to the event store.
-resource "google_project_service" "cloud_functions" {
-  project = var.google_cloud_project_id
-  service = "cloudfunctions.googleapis.com"
-}
-
-
-# Cloud Run runs the Twined services as docker containers.
-resource "google_project_service" "cloud_run" {
-  project = var.google_cloud_project_id
-  service = "run.googleapis.com"
-}
-
-
-# IAM provides fine-grained authentication and authorisation to use and access the Twined services and input/output data.
-resource "google_project_service" "iam" {
-  project = var.google_cloud_project_id
-  service = "iam.googleapis.com"
-}
-
-
-# Pub/Sub is the transport mechanism for Twined service events.
-resource "google_project_service" "pub_sub" {
-  project = var.google_cloud_project_id
-  service = "pubsub.googleapis.com"
+  disable_dependent_services = True
 }
