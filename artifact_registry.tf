8923490931b100301f1cd22541cf01b87bd7e8d6
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 17:09:10 +0000

    ENH: Improve name and description of artifact registry

--- artifact_registry.tf
+++ artifact_registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "service_docker_images" {
-  repository_id = "${var.twined_service_namespace}_twined_services"
-  description   = "Docker image repository"
+  repository_id = "${var.twined_service_namespace}_octue_twined_services"
+  description   = "Docker image repository for Octue Twined services"
   format        = "DOCKER"
   location      = var.google_cloud_region
 }
