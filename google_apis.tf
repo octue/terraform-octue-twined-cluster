e7caf3eff543755cebd3522078bef06800dde442
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 12:44:30 +0000

    ENH: Define Google APIs used by terraform module

---
+++ google_apis.tf
@@ -0,0 +1,40 @@
+# Artifact Registry is used to store docker images of the Octue services.
+resource "google_project_service" "artifact_registry" {
+  project = var.project
+  service = "artifactregistry.googleapis.com"
+}
+
+
+# BigQuery provides the event store for Octue service events (questions, results, log messages etc.).
+resource "google_project_service" "bigquery" {
+  project = var.project
+  service = "bigquery.googleapis.com"
+}
+
+
+# Cloud Functions runs the event handler that directs Octue service events to the event store.
+resource "google_project_service" "cloud_functions" {
+  project = var.project
+  service = "cloudfunctions.googleapis.com"
+}
+
+
+# Cloud Run runs the Octue services as docker containers.
+resource "google_project_service" "cloud_run" {
+  project = var.project
+  service = "run.googleapis.com"
+}
+
+
+# IAM provides fine-grained authentication and authorisation to use and access the Octue services and input/output data.
+resource "google_project_service" "iam" {
+  project = var.project
+  service = "iam.googleapis.com"
+}
+
+
+# Pub/Sub is the transport mechanism for Octue service events.
+resource "google_project_service" "pub_sub" {
+  project = var.project
+  service = "pubsub.googleapis.com"
+}
