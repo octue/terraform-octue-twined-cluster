7a834283727ffff2ebb8ca50876657b5f4bc21ef
cortadocodes <cortado.codes@protonmail.com>
2024-11-21 18:33:14 +0000

    FIX: Make bigquery, pub/sub, and artifact registry wait for APIs

--- artifact_registry.tf
+++ artifact_registry.tf
@@ -3,4 +3,5 @@ resource "google_artifact_registry_repository" "service_docker_images" {
   description   = "Docker image repository for Octue Twined services"
   format        = "DOCKER"
   location      = var.google_cloud_region
+  depends_on   = [time_sleep.wait_for_google_apis_to_enable]
 }
--- pub_sub.tf
+++ pub_sub.tf
@@ -1,3 +1,4 @@
 resource "google_pubsub_topic" "services_topic" {
   name = "octue.services"
+  depends_on   = [time_sleep.wait_for_google_apis_to_enable]
 }
