5f22ab937f257d6e0d9156b76d75b74894feefe9
cortadocodes <cortado.codes@protonmail.com>
2024-11-23 12:46:07 +0000

    REF: Factor out IAM roles for service accounts

--- iam_bindings.tf
+++ iam_bindings.tf
@@ -7,60 +7,53 @@ locals {
     ["serviceAccount:${google_service_account.github_actions_service_account.email}"],
     local.admin_service_account_emails
   )
+
+  roles_all = [
+    "roles/iam.serviceAccountUser",
+    "roles/pubsub.editor",
+    "roles/errorreporting.writer"
+  ]
+
+  roles_github_actions = [
+    # Allows the GHA to call "namespaces get" for Cloud Run to determine the resulting run URLs of the services.
+    # This should also allow a service to get its own name by using:
+    #   https://stackoverflow.com/questions/65628822/google-cloud-run-can-a-service-know-its-own-url/65634104#65634104
+    "roles/run.developer",
+    "roles/artifactregistry.writer",
+    "roles/storage.objectAdmin"
+  ]
+
+  roles_admin_accounts = [
+    "roles/bigquery.dataViewer",
+    "roles/bigquery.jobUser",
+    "roles/bigquery.readSessionUser",
+  ]
 }
 
 
-resource "google_project_iam_binding" "iam_serviceaccountuser" {
+resource "google_project_iam_binding" "iam_all" {
+  for_each = local.roles_all
   project = var.google_cloud_project_id
-  role    = "roles/iam.serviceAccountUser"
+  role    = each.key
   members = local.all_service_account_emails
-}
-
-
-resource "google_project_iam_binding" "pubsub_editor" {
-  project = var.google_cloud_project_id
-  role    = "roles/pubsub.editor"
-  members = local.all_service_account_emails
-}
-
-
-# Allows the GHA to call "namespaces get" for Cloud Run to determine the resulting run URLs of the services.
-# This should also allow a service to get its own name by using:
-#   https://stackoverflow.com/questions/65628822/google-cloud-run-can-a-service-know-its-own-url/65634104#65634104
-resource "google_project_iam_binding" "run_developer" {
-  project = var.google_cloud_project_id
-  role    = "roles/run.developer"
-  members = [
-    "serviceAccount:${google_service_account.github_actions_service_account.email}",
-  ]
   depends_on = [time_sleep.wait_for_google_apis_to_enable]
 }
 
 
-resource "google_project_iam_binding" "artifactregistry_writer" {
-  project = var.google_cloud_project_id
-  role    = "roles/artifactregistry.writer"
-  members = [
-    "serviceAccount:${google_service_account.github_actions_service_account.email}",
-  ]
-  depends_on = [time_sleep.wait_for_google_apis_to_enable]
-}
-
-
-resource "google_project_iam_binding" "storage_objectadmin" {
-  project = var.google_cloud_project_id
-  role    = "roles/storage.objectAdmin"
-  members = [
-    "serviceAccount:${google_service_account.github_actions_service_account.email}",
-  ]
-  depends_on = [time_sleep.wait_for_google_apis_to_enable]
-}
-
-
-resource "google_project_iam_binding" "errorreporting_writer" {
+resource "google_project_iam_binding" "iam_admin_accounts" {
+  for_each = local.roles_admin_accounts
   project    = var.google_cloud_project_id
-  role       = "roles/errorreporting.writer"
-  members    = local.all_service_account_emails
+  role       = each.key
+  members    = local.admin_service_account_emails
+  depends_on = [time_sleep.wait_for_google_apis_to_enable]
+}
+
+
+resource "google_project_iam_binding" "iam_github_actions" {
+  for_each = local.roles_github_actions
+  project = var.google_cloud_project_id
+  role    = each.key
+  members = ["serviceAccount:${google_service_account.github_actions_service_account.email}"]
   depends_on = [time_sleep.wait_for_google_apis_to_enable]
 }
 
@@ -71,29 +64,5 @@ resource "google_project_iam_binding" "bigquery_dataeditor" {
   members = [
     "serviceAccount:service-${var.google_cloud_project_number}@gcp-sa-pubsub.iam.gserviceaccount.com",
   ]
-  depends_on = [time_sleep.wait_for_google_apis_to_enable]
-}
-
-
-resource "google_project_iam_binding" "bigquery_dataviewer" {
-  project    = var.google_cloud_project_id
-  role       = "roles/bigquery.dataViewer"
-  members    = local.admin_service_account_emails
-  depends_on = [time_sleep.wait_for_google_apis_to_enable]
-}
-
-
-resource "google_project_iam_binding" "bigquery_jobuser" {
-  project    = var.google_cloud_project_id
-  role       = "roles/bigquery.jobUser"
-  members    = local.admin_service_account_emails
-  depends_on = [time_sleep.wait_for_google_apis_to_enable]
-}
-
-
-resource "google_project_iam_binding" "bigquery_readsessionuser" {
-  project    = var.google_cloud_project_id
-  role       = "roles/bigquery.readSessionUser"
-  members    = local.admin_service_account_emails
   depends_on = [time_sleep.wait_for_google_apis_to_enable]
 }
