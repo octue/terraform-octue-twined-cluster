4260bfbca3e03cf93d3a85261bac669f4ada960b
cortadocodes <cortado.codes@protonmail.com>
2025-01-20 10:32:04 +0000

    FIX: Make service accounts compatible with workspaces

--- iam_service_accounts.tf
+++ iam_service_accounts.tf
@@ -1,7 +1,7 @@
 resource "google_service_account" "github_actions_service_account" {
-  account_id   = "github-actions"
-  description  = "Allow GitHub Actions to test and deploy Octue Twined services."
-  display_name = "github-actions"
+  account_id   = "${var.environment}-github-actions"
+  description  = "Allow GitHub Actions to test and deploy Octue Twined services in the ${var.environment} environment."
+  display_name = "${var.environment}-github-actions"
   project      = var.google_cloud_project_id
   depends_on   = [time_sleep.wait_for_google_apis_to_enable]
 }
@@ -9,9 +9,9 @@ resource "google_service_account" "github_actions_service_account" {
 
 resource "google_service_account" "admin_accounts" {
   for_each     = var.service_account_names
-  account_id   = each.key
-  display_name = each.key
+  account_id   = "${var.environment}-${each.key}"
+  display_name = "${var.environment}-${each.key}"
   project      = var.google_cloud_project_id
-  description  = "Allow ${each.key} to access most resources related to Octue Twined services"
+  description  = "Allow ${each.key} to access most resources related to Octue Twined services in the ${var.environment} environment."
   depends_on   = [time_sleep.wait_for_google_apis_to_enable]
 }
