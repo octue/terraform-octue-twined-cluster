f9c981d176accaacf8708255ba83a968397082d0
cortadocodes <cortado.codes@protonmail.com>
2025-01-22 15:53:42 +0000

    ENH: Add `services_topic` output

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,9 @@ output "event_store" {
   description = "The full ID of the BigQuery table acting as the Twined services event store."
   value       = google_bigquery_table.service_event_table.table_id
 }
+
+
+output "services_topic" {
+  description = "The Pub/Sub topic that all Octue Twined service events are published to."
+  value = google_pubsub_topic.services_topic.name
+}
