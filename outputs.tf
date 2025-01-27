ade209f877ac8206c71ef3d1cf41921c828b227c
cortadocodes <cortado.codes@protonmail.com>
2025-01-27 11:04:08 +0000

    ENH: Add `kubernetes_cluster` output

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,8 @@ output "services_topic" {
   description = "The Pub/Sub topic that all Octue Twined service events are published to."
   value = google_pubsub_topic.services_topic.name
 }
+
+
+output "kubernetes_cluster" {
+  value = google_container_cluster.primary
+}
