197a0039dd6065b2b9ff3269bd5c00ed6bf78ebd
cortadocodes <cortado.codes@protonmail.com>
2025-01-29 14:34:45 +0000

    FIX: Add workload identity pool policy for GKE

--- iam_kubernetes.tf
+++ iam_kubernetes.tf
@@ -33,9 +33,21 @@ resource "google_project_iam_member" "default_node_service_account" {
 }
 
 
-# gcloud iam service-accounts add-iam-policy-binding GSA_NAME@PROJECT_ID.iam.gserviceaccount.com \
-#     --role roles/iam.workloadIdentityUser \
-#     --member "GSA_NAME:PROJECT_ID.svc.id.goog[KSA_NAME]"
+data "google_iam_policy" "gke_workload_identity_pool_policy" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+    members = [
+      "${data.google_project.project.number}-compute@developer.gserviceaccount.com:${var.google_cloud_project_id}.svc.id.goog[${kubernetes_service_account.kubernetes_google_wif_service_account.metadata[0].name}]"
+    ]
+  }
+}
+
+
+# Allow a machine under Workload Identity Federation to act as the given service account.
+resource "google_service_account_iam_policy" "gke_workload_identity_service_account_policy" {
+  policy_data        = data.google_iam_policy.gke_workload_identity_pool_policy.policy_data
+  service_account_id = "projects/${var.google_cloud_project_id}/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
 
 
 resource "kubernetes_service_account" "kubernetes_google_wif_service_account" {
