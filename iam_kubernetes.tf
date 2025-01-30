6f8f90e0980d94797b0fa516819faeb0c0754d3f
cortadocodes <cortado.codes@protonmail.com>
2025-01-30 09:33:45 +0000

    WIP: Remove GKE WIF resources

--- iam_kubernetes.tf
+++ iam_kubernetes.tf
@@ -19,36 +19,9 @@ resource "google_project_iam_member" "default_node_service_account" {
 }
 
 
-data "google_iam_policy" "gke_workload_identity_pool_policy" {
-  binding {
-    role = "roles/iam.workloadIdentityUser"
-    members = [
-      "${data.google_project.project.number}-compute@developer.gserviceaccount.com:${var.google_cloud_project_id}.svc.id.goog[${kubernetes_service_account.kubernetes_google_wif_service_account.metadata[0].name}]"
-    ]
-  }
-}
-
-
-# Allow a machine under Workload Identity Federation to act as the given service account.
-resource "google_service_account_iam_policy" "gke_workload_identity_service_account_policy" {
-  policy_data        = data.google_iam_policy.gke_workload_identity_pool_policy.policy_data
-  service_account_id = "projects/${var.google_cloud_project_id}/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
-
-resource "kubernetes_service_account" "kubernetes_google_wif_service_account" {
-  metadata {
-    name = "google-workload-identity-federation"
-    annotations = {
-      "iam.gke.io/gcp-service-account" = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-    }
-  }
-}
-
-
-resource "google_project_iam_member" "kubernetes_wif_roles" {
+resource "google_project_iam_member" "default_node_service_account" {
   count = length(local.cluster_iam_roles)
   project = var.google_cloud_project_id
   role    = local.cluster_iam_roles[count.index]
-  member  = "principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.google_cloud_project_id}.svc.id.goog/subject/ns/default/sa/${kubernetes_service_account.kubernetes_google_wif_service_account.metadata[0].name}"
+  member  = "serviceAccount:${google_service_account.kubernetes_node_service_account.email}"
 }
