af3ea5aab50c66f89f2eaa43141b6c7852e4c226
cortadocodes <cortado.codes@protonmail.com>
2025-02-03 14:53:46 +0000

    WIP: Try event handler with hard-coded k8s service account

--- functions.tf
+++ functions.tf
@@ -9,7 +9,7 @@ resource "google_cloudfunctions2_function" "event_handler" {
     source {
       storage_source {
         bucket = "twined-gcp"
-        object = "event_handler/0.7.0-rc.10.zip"
+        object = "event_handler/0.7.0-rc.11.zip"
       }
     }
   }
