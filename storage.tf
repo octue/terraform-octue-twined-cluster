72a11f07f8c3599e3c8aa9d1e0943eaa0be32da2
cortadocodes <cortado.codes@protonmail.com>
2025-02-03 17:03:54 +0000

    FEA: Provide a cloud storage bucket

---
+++ storage.tf
@@ -0,0 +1,11 @@
+resource "google_storage_bucket" "default" {
+  name     = "${var.environment}-${var.github_organisation}-octue-twined"
+  location = var.google_cloud_region
+
+  autoclass {
+    enabled                = true
+    terminal_storage_class = "ARCHIVE"
+  }
+
+  force_destroy = !var.deletion_protection
+}
