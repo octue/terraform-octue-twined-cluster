4f3e0c41299ca6de4a368644c507c0d5ee0f06d7
cortadocodes <cortado.codes@protonmail.com>
2025-02-03 10:45:30 +0000

    ENH: Add kubernetes service account and roles for WIF

--- iam_roles_kubernetes.tf
+++ iam_roles_kubernetes.tf
@@ -18,3 +18,11 @@ resource "google_project_iam_member" "kubernetes__roles" {
   member     = "serviceAccount:${google_service_account.kubernetes.email}"
   depends_on = [time_sleep.wait_for_google_apis_to_enable]
 }
+
+
+resource "google_project_iam_member" "kubernetes_service_account__roles" {
+  for_each = local.kubernetes_node_roles
+  project  = var.google_cloud_project_id
+  role     = each.value
+  member   = "principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.google_cloud_project_id}.svc.id.goog/subject/ns/default/sa/${kubernetes_service_account.default.metadata[0].name}"
+}
--- kubernetes_cluster.tf
+++ kubernetes_cluster.tf
@@ -18,3 +18,10 @@ resource "time_sleep" "wait_for_cluster_to_be_ready" {
   depends_on      = [google_container_cluster.primary]
   create_duration = "2m"
 }
+
+
+resource "kubernetes_service_account" "default" {
+  metadata {
+    name = "${var.environment}-octue-twined-kubernetes-service-account"
+  }
+}
