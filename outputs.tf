35c6a9dd08d6c7e824c48bef7b09ea9f8170edd1
cortadocodes <cortado.codes@protonmail.com>
2025-02-04 15:43:31 +0000

    ENH: Add service registry URL as output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,12 @@ output "event_store" {
 }
 
 
+output "service_registry" {
+  description = "The URL of the service registry."
+  value       = google_cloudfunctions2_function.service_registry.url
+}
+
+
 output "services_topic" {
   description = "The Pub/Sub topic that all Octue Twined service events are published to."
   value       = google_pubsub_topic.services_topic.name
