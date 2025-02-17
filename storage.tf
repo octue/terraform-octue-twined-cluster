b549e25b119783dacfb91d60eb844ab6f2bcea34
cortadocodes <cortado.codes@protonmail.com>
2025-02-17 15:35:53 +0000

    FIX: Ensure bucket names are unique

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "default" {
-  name                        = "${var.environment}-${var.github_organisation}-octue-twined"
+  name                        = "${var.environment}-${var.github_organisation}-${var.google_cloud_project_id}-octue-twined"
   location                    = var.google_cloud_region
   uniform_bucket_level_access = true
 
