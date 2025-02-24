59ce0bdae44b0248262ff231d48672319ddcb68f
cortadocodes <cortado.codes@protonmail.com>
2025-02-24 12:15:51 +0000

    REF: Combine IAM config files

--- iam_kubernetes.tf
+++ iam_kubernetes.tf
@@ -12,6 +12,15 @@ locals {
 }
 
 
+resource "google_service_account" "kubernetes" {
+  account_id   = "${var.environment}-octue-twined-kubernetes"
+  display_name = "${var.environment}-octue-twined-kubernetes"
+  project      = var.google_cloud_project_id
+  description  = "Allow the Octue Twined Kubernetes cluster to access resources in the ${var.environment} environment."
+  depends_on   = [time_sleep.wait_for_google_apis_to_enable]
+}
+
+
 resource "google_project_iam_member" "kubernetes__roles" {
   for_each   = local.kubernetes_node_roles
   project    = var.google_cloud_project_id
