acd6a1838ef70632412a5ad5536252b9ac83c431
cortadocodes <cortado.codes@protonmail.com>
2025-03-04 10:50:36 +0000

    ENH: Remove `allUsers` invoker role for event handler

--- iam_functions.tf
+++ iam_functions.tf
@@ -1,12 +1,3 @@
-resource "google_cloud_run_service_iam_member" "event_handler_invoker" {
-  service    = google_cloudfunctions2_function.event_handler.name
-  location   = google_cloudfunctions2_function.event_handler.location
-  role       = "roles/run.invoker"
-  member     = "allUsers"
-  depends_on = [time_sleep.wait_for_google_apis_to_enable]
-}
-
-
 resource "google_cloud_run_service_iam_member" "service_registry_invoker" {
   service    = google_cloudfunctions2_function.service_registry.name
   location   = google_cloudfunctions2_function.service_registry.location
