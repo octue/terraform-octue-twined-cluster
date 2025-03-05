f6ab2ade7212e981c2dc93b69e39e354c8b366aa
cortadocodes <cortado.codes@protonmail.com>
2025-03-05 11:51:11 +0000

    DOC: Add missing output description

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
 output "service_registry" {
-  description = "The URL of the service registry."
   value       = google_cloudfunctions2_function.service_registry.url
+  description = "The URL of the service registry."
 }
 
 
 output "services_topic" {
-  description = "The Pub/Sub topic that all Octue Twined service events are published to."
   value       = google_pubsub_topic.services_topic.name
+  description = "The Pub/Sub topic that all Twined service events are published to."
 }
 
 
 output "kubernetes_cluster" {
-  value = google_container_cluster.primary
+  value       = google_container_cluster.primary
+  description = "The Kubernetes cluster that Twined service containers are run on."
 }
